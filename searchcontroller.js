e09c6b0c051b6d6427f47f956ab3522dc43483da
rohits9 <rohit.s9@tcs.com>
2015-12-04 14:45:51 +0530

    Update searchcontroller.js

--- searchcontroller.js
+++ searchcontroller.js
@@ -3,13 +3,16 @@ github.controller('searchController', function($scope, $http) {
 	$scope.findRepo = function (searchTerm) {
 		$http({
 		  method: 'GET',
-		  url: 'https://api.github.com/search'+'/repositories?callback=JSON_CALLBACK&q='+searchTerm
+		  url: 'https://api.github.com/search'+'/users?callback=JSON_CALLBACK&q='+searchTerm
 		}).then(function successCallback(response) {
 		    var data = response.data.replace("/**/JSON_CALLBACK(", "[");
 		    var finalData = data.replace("})", "}]");
 		    $scope.result = JSON.parse(finalData);
 		    $scope.searchResult = $scope.result[0].data;
 		    
+		    if($scope.searchResult.items.length == 0){
+		    	$scope.errMsg = "No records found.";
+		    }
 		    $("#myButton4").button('complete');
 		}, function errorCallback(response) {
 		    // called asynchronously if an error occurs
