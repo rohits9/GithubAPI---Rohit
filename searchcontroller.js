4a181f6d7011ad09c640e72ee7e5553f6f0ceb07
rohits9 <rohit.s9@tcs.com>
2015-12-04 13:08:45 +0530

    Create searchcontroller.js

---
+++ searchcontroller.js
@@ -0,0 +1,28 @@
+github.controller('searchController', function($scope, $http) {
+	
+	$scope.findRepo = function (searchTerm) {
+		$http({
+		  method: 'GET',
+		  url: 'https://api.github.com/search'+'/repositories?callback=JSON_CALLBACK&q='+searchTerm
+		}).then(function successCallback(response) {
+		    var data = response.data.replace("/**/JSON_CALLBACK(", "[");
+		    var finalData = data.replace("})", "}]");
+		    $scope.result = JSON.parse(finalData);
+		    $scope.searchResult = $scope.result[0].data;
+		    
+		    $("#myButton4").button('complete');
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		});
+	}
+	
+	$scope.init = function(){
+
+	}
+	$scope.init();
+	
+	$("#myButton4").click(function(){
+		$(this).html('Loading...');
+	});        
+});
